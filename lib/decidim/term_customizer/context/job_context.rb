e109f873b11f1820e70f8707db5468ce34b49cd2
Ivan Vergés <ivan@pokecode.net>
2026-04-21 12:38:41 +0200

    increase component detection

--- lib/decidim/term_customizer/context/job_context.rb
+++ lib/decidim/term_customizer/context/job_context.rb
@@ -5,7 +5,6 @@ module Decidim
     module Context
       class JobContext < Base
         # rubocop:disable Metrics/CyclomaticComplexity
-        # rubocop:disable Metrics/PerceivedComplexity
         def resolve!
           # Figure out the organization and user through the job arguments if
           # passed for the job.
@@ -14,7 +13,7 @@ module Decidim
             @organization ||= organization_from_argument(arg)
             @space ||= space_from_argument(arg)
             @component ||= component_from_argument(arg)
-            user ||= arg if arg.is_a?(Decidim::User)
+            user ||= find_object_by_class(arg, Decidim::User)
           end
 
           # In case a component was found, define the space as the component
@@ -30,26 +29,46 @@ module Decidim
           @organization ||= user.organization if user
         end
         # rubocop:enable Metrics/CyclomaticComplexity
-        # rubocop:enable Metrics/PerceivedComplexity
 
         protected
 
         def organization_from_argument(arg)
-          return arg if arg.is_a?(Decidim::Organization)
+          org = find_object_by_class(arg, Decidim::Organization)
 
-          arg.organization if arg.respond_to?(:organization)
+          org || arg.try(:organization)
         end
 
         def space_from_argument(arg)
-          return arg if arg.is_a?(Decidim::Participable)
+          space = find_object_by_class(arg, Decidim::Participable)
 
-          arg.participatory_space if arg.respond_to?(:participatory_space)
+          space || arg.try(:participatory_space)
         end
 
         def component_from_argument(arg)
-          return arg if arg.is_a?(Decidim::Component)
+          component = find_object_by_class(arg, Decidim::Component)
+          return component if component
+          return arg.component if arg.respond_to?(:component)
 
-          arg.component if arg.respond_to?(:component)
+          questionnaire_component = find_object_by_class(arg, Decidim::Forms::Questionnaire)&.questionnaire_for
+          return questionnaire_component if questionnaire_component.is_a?(Decidim::Component)
+          return questionnaire_component.component if questionnaire_component.respond_to?(:component)
+
+          nil
+        end
+
+        def find_object_by_class(obj, klass)
+          return obj if obj.is_a?(klass)
+
+          return find_object_by_class(obj.values, klass) if obj.respond_to?(:values)
+
+          if obj.respond_to?(:each)
+            obj.each do |item|
+              found = find_object_by_class(item, klass)
+              return found if found
+            end
+          end
+
+          nil
         end
       end
     end
