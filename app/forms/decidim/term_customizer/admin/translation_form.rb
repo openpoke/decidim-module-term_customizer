3fa7933ddd84085aa84fbbc147b811ecfbb5da08
Ivan Vergés <ivan@pokecode.net>
2026-05-08 18:34:33 +0200

    prevent introducing wrong keys as leaf/children

--- app/forms/decidim/term_customizer/admin/translation_form.rb
+++ app/forms/decidim/term_customizer/admin/translation_form.rb
@@ -17,6 +17,7 @@ module Decidim
         validates :key, format: { with: %r{\A([A-Za-z0-9_/?-]+\.)*[A-Za-z0-9_/?-]+\z} }, unless: -> { key.blank? }
         validates :value, translatable_presence: true
         validate :key_uniqueness
+        validate :key_hierarchy_uniqueness, unless: -> { key.blank? }
 
         def map_model(model)
           self.value = Decidim::TermCustomizer::Translation.where(
@@ -32,6 +33,20 @@ module Decidim
             locale: I18n.locale,
             key:
           ).where.not(id:).exists?
+        end
+
+        def key_hierarchy_uniqueness
+          return unless translation_set
+
+          scope = translation_set.translations.where(locale: I18n.locale).where.not(id:)
+          key_parts = key.split(".")
+          ancestors = key_parts.each_index.map { |index| key_parts[0..index].join(".") }[0...-1]
+          escaped_key = ActiveRecord::Base.sanitize_sql_like(key)
+
+          has_ancestor = ancestors.any? && scope.exists?(key: ancestors)
+          has_descendant = scope.exists?(["key LIKE ?", "#{escaped_key}.%"])
+
+          errors.add(:key, :invalid) if has_ancestor || has_descendant
         end
       end
     end
