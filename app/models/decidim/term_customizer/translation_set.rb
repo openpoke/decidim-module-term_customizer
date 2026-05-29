fa8a72898ee83bb42a49bbd871a958d8c1c7bdf1
elviabth <ejbenedith@gmail.com>
2026-05-29 09:13:43 +0200

    add search to translations sets

--- app/models/decidim/term_customizer/translation_set.rb
+++ app/models/decidim/term_customizer/translation_set.rb
@@ -3,6 +3,8 @@
 module Decidim
   module TermCustomizer
     class TranslationSet < TermCustomizer::ApplicationRecord
+      include Decidim::FilterableResource
+
       self.table_name = "decidim_term_customizer_translation_sets"
 
       has_many :translations,
@@ -12,6 +14,18 @@ module Decidim
       has_many :constraints,
                class_name: "Decidim::TermCustomizer::Constraint",
                dependent: :destroy
+
+      # Create i18n ransackers for :name and :description.
+      # Create the :search_text ransacker alias for searching from both of these.
+      ransacker_i18n_multi :search_text, [:name]
+
+      def self.ransackable_attributes(_auth_object = nil)
+        %w(search_text)
+      end
+
+      def self.ransackable_associations(_auth_object = nil)
+        []
+      end
     end
   end
 end
